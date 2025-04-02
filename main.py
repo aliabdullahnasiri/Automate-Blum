6a20e533bd053e953abcc746554c6ff63a4b03f6
Ali Abdullah Nasiri <105828464+aliabdullahnasiri@users.noreply.github.com>
2025-04-02 20:53:25 +0430

    Main.py file updated.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import threading
+from typing import List
 
 from rich.console import Console
 from rich.traceback import install
@@ -12,6 +14,14 @@ console: Console = Console()
 telegram: Telegram = Telegram()
 
 if web_data := asyncio.run(telegram.get_web_data("BlumCryptoBot", "app")):
+    threads: List[threading.Thread] = []
+
     for data in web_data:
         blum: Blum = Blum(data)
-        blum.main()
+        threads.append(threading.Thread(target=blum.main))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
