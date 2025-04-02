6a41cee993acc3b631df5a60f62e3608a6ec7590
Ali Abdullah Nasiri <105828464+aliabdullahnasiri@users.noreply.github.com>
2025-04-02 19:19:28 +0430

    Config.py file added.

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# if you need play in the game
+PLAY_GAME = True
+
+# points with each play game; max 280
+POINTS = [240, 280]
+
+# True - if need shuffle tasks or else - False
+SHUFFLE_TASKS = False
+
+DEBUG: bool = True
